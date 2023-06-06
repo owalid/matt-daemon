cb3314c11e42f5e1f693fa424ece2a0732cb5175
owalid <othmane.el.ayadi@gmail.com>
2023-06-06 19:44:09 +0200

    first version of daemonize

--- includes/Mattdaemon.hpp
+++ includes/Mattdaemon.hpp
@@ -2,8 +2,18 @@
 #ifndef MATTDAEMON_HPP
 # define MATTDAEMON_HPP
 
+// Global includes libraries
 #include <cstdlib>
 #include <iomanip>
 #include <iostream>
+
+// For daemonize
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <syslog.h>
 
 #endif
--- src/Mattdaemon.cpp
+++ src/Mattdaemon.cpp
@@ -1,7 +1,37 @@
 #include "Mattdaemon.hpp"
 
+void check_pid(pid_t pid)
+{
+  if (pid < 0)
+    exit(1);
+  if (pid > 0)
+    exit(0);
+}
+
+void daemonize()
+{
+  pid_t pid, sid;
+
+  pid = fork(); // Parent process will terminated, the child process now runs in the background.
+  check_pid(pid);
+  
+  sid = setsid(); // Create a new session.
+  if (sid < 0)
+    exit(1);
+
+  pid = fork(); // Fork again to ensure that the daemon never reacquires a controlling terminal. (PID != SID)
+  check_pid(pid);
+
+  // Close all open file descriptors
+  close(STDIN_FILENO);
+  close(STDOUT_FILENO);
+  close(STDERR_FILENO);
+}
+
 int main(int argc, char *argv[])
 {
-  std::cout << "hello world from mattdeamon" << '\n';
+  daemonize();
+  while (1)
+    ;
   return 0;
 }
