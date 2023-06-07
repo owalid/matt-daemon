16bb1cff3e6c898d1abdfa5e46b6ff27b9ca9d4f
owalid <othmane.el.ayadi@gmail.com>
2023-06-07 14:36:38 +0200

    first version of daemonize (#21)

--- includes/Mattdaemon.hpp
+++ includes/Mattdaemon.hpp
@@ -2,9 +2,19 @@
 #ifndef MATTDAEMON_HPP
 # define MATTDAEMON_HPP
 
+// Global includes libraries
 #include <cstdlib>
 #include <iomanip>
 #include <iostream>
 #include <unistd.h>
+
+// For daemonize
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <syslog.h>
 
 #endif
