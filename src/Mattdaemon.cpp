93269c42799214a3edc906c639ce78a8a4b90998
owalid <othmane.el.ayadi@gmail.com>
2023-06-07 14:26:11 +0200

    add check for root user (#20)

--- src/Mattdaemon.cpp
+++ src/Mattdaemon.cpp
@@ -2,6 +2,12 @@
 
 int main(int argc, char *argv[])
 {
+  // checl if the user is root
+  if (getuid() != 0)
+  {
+    std::cout << "You must be root to run this program" << '\n';
+    return 1;
+  }
   std::cout << "hello world from mattdeamon" << '\n';
   return 0;
 }
