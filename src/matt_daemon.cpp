4dea8cc6cb0488bd86074fba3a31fbf2867c2941
owalid <othmane.el.ayadi@gmail.com>
2023-06-12 22:07:03 +0200

    feat: add quit command

--- src/matt_daemon.cpp
+++ src/matt_daemon.cpp
@@ -115,11 +115,22 @@ int main(int argc, char *argv[])
             }
             else
             {
-              logger.MakeNewEvent(logger.GetCategoryFromEnum(log), logger.GetEventFromEnum(userRequest), buffer);
-              // Ici on devra traiter les données.
-              //clear du buffer.
-              memset(&buffer, 0, sizeof(buffer));
+              if (strcmp(buffer, "quit\n") == 0)
+              {
+                logger.MakeNewEvent(logger.GetCategoryFromEnum(info), logger.GetEventFromEnum(programQuit), "");
+                close(fd);
+                memset(&buffer, 0, sizeof(buffer));  
+                FD_CLR(fd, &srv.master_fd_);
+                srv.SetNumberOfConnectedCli(srv.GetNumberOfConnectedCli() - 1);
+                exit(0);
+              }
+              else
+              {
+                logger.MakeNewEvent(logger.GetCategoryFromEnum(log), logger.GetEventFromEnum(userInput), buffer);
+              }
             }
+            // clear du buffer.
+            memset(&buffer, 0, sizeof(buffer));
           }
         }
       }
