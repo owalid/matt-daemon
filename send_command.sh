c303d955842a21fc40e067b0e91ac46a82f21847
owalid <othmane.el.ayadi@gmail.com>
2023-06-14 22:46:01 +0200

    add usage for send_command.sh

--- send_command.sh
+++ send_command.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
+
+if [ $# -eq 0 ]
+  then
+    echo "Usage: send_command.sh <clear | quit>"
+    exit 1
+fi
+
+if [ $1 != "clear" ] && [ $1 != "quit" ]
+  then
+    echo "Usage: send_command.sh <clear | quit>"
+    exit 1
+fi
+
 echo $1 | nc -N 127.0.0.1 4242
