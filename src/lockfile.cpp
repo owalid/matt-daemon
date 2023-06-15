6fcf6ad9ce33ffbea89d8eddba4ed39f5500ed67
kibatche <61985948+kibatche@users.noreply.github.com>
2023-06-15 13:08:45 +0200

    Ajout d'un log quand on quitte à cause d'une erreur et mise sous thro… (#40)
    * Ajout d'un log quand on quitte à cause d'une erreur et mise sous throw quand on a une erreur de lockfile.
    
    * suppression d'un affichage de débogage
    
    ---------
    
    Co-authored-by: kibatche <kibatche@kibatche>

--- src/lockfile.cpp
+++ src/lockfile.cpp
@@ -4,20 +4,10 @@ int CreateLockFile(TintinReporter &logger)
 {
   int fd_lock = open(LOCKFILE_PATH, O_RDWR | O_CREAT, 0600);
 
-  std::cout << "fd_lock = " << fd_lock << std::endl;
   if (fd_lock == -1)
-  {
-    std::cerr << "Unable to create or read the lock file. The program will exit." << std::endl;
-    exit(EXIT_FAILURE);
-  }
+    throw std::runtime_error("Unable to create or read the lock file. The program will exit.");
   if (flock(fd_lock, LOCK_EX | LOCK_NB) == -1)
-  {
-    std::cerr << "Lockfile locked by another process." << std::endl;
-    logger.MakeNewEvent(logger.GetCategoryFromEnum(error), logger.GetEventFromEnum(errorFileLock), "");
-    logger.MakeNewEvent(logger.GetCategoryFromEnum(error), logger.GetEventFromEnum(programQuit), \
-      " Failure : " + std::to_string(EXIT_FAILURE));
-    exit(EXIT_FAILURE);
-  }
+    throw std::runtime_error("Lockfile locked by another process.");
   return fd_lock;
 }
 
@@ -30,7 +20,7 @@ void ReleaseLockFile(TintinReporter &logger, int fd_lock)
     std::cerr << "Lockfile cannot be unlocked. This is a hard failure." << std::endl;
     logger.MakeNewEvent(logger.GetCategoryFromEnum(error), logger.GetEventFromEnum(errorFileUnlock), "");
     logger.MakeNewEvent(logger.GetCategoryFromEnum(error), logger.GetEventFromEnum(programQuit), \
-      " Failure : " + std::to_string(EXIT_FAILURE));
+      " Failure : " + std::string("Lockfile cannot be unlocked. This is a hard failure."));
     std::remove(LOCKFILE_PATH);
     exit(EXIT_FAILURE);
   }
